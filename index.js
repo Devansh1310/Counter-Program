89053297d1402791280a88a780191421bf054419
Devansh Bhardwaj <104239369+Devansh1310@users.noreply.github.com>
2024-07-30 14:25:17 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+const descreaseBTN = document.getElementById("descreaseBTN")
+const resetBTN = document.getElementById("resetBTN")
+const increaseBTN = document.getElementById("increaseBTN")
+const countlabel = document.getElementById("countlabel")
+
+let count = 0;
+
+increaseBTN.onclick = function(){
+    count++;
+    countlabel.textContent = count;
+}
+
+descreaseBTN.onclick = function(){
+    count--;
+    countlabel.textContent = count;
+}
+
+resetBTN.onclick = function(){
+    count = 0;
+    countlabel.textContent = count;
+}
